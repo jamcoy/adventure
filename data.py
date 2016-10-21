2fbe5e08e8f00e7115cca37f155b57e377b6645d
jamcoy <james@coynemail.net>
2016-10-21 18:12:00 +0100

    Add location descriptions and collectable items

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 locations = {
-    (0, 0): 'house',
-    (0, 1): 'lake',
-    (1, 0): 'park',
-    (1, 1): 'market',
-}
+    (0, 0): {'title': 'house', 'description': 'The garden is overgrown and the house looks rundown.', 'item': 'bicycle'},
+    (0, 1): {'title': 'lake', 'description': 'The water looks very cold. You do not fancy a swim.', 'item': 'duck'},
+    (1, 0): {'title': 'park', 'description': 'There are lots of big trees. A squirrel says "hello".', 'item': 'squirrel'},
+    (1, 1): {'title': 'market', 'description': 'You are stood by a market stall selling fruit.', 'item': 'banana'}
+    }
